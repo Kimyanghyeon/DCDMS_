a4139b0b6db858e53af649e455ef35b1a0236f09
KimYangHyeon <xiu9003260@gmail.com>
2022-11-15 20:59:53 +0900

    Start - Initial Settings

---
+++ YangHyeon_Spring/src/main/java/kr/inhatc/spring/YangHyeonSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.inhatc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangHyeonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangHyeonSpringApplication.class, args);
+	}
+
+}
---
+++ YangHyeon_Spring/src/main/java/kr/inhatc/spring/test/controller/TestController.java
@@ -0,0 +1,29 @@
+package kr.inhatc.spring.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.inhatc.spring.test.dto.TestDto;
+
+@RestController
+public class TestController {
+	
+	@GetMapping(value="/")
+	public String hello() {
+		
+		return "Hello World";
+		
+	}//end of hello
+	
+	
+	@GetMapping(value = "/test")
+	public TestDto test() {
+		
+		TestDto dto = new TestDto();
+		dto.setName("김양현씨 지금 존나게 테스팅 중입니다");
+		dto.setAge(20);
+		return dto;
+		
+	}//end of TestDto
+
+}//end of class
